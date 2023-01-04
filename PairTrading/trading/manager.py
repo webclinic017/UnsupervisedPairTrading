b271302a6754f5ffdbed1ac283dbcba652a09218
John Xu <haoxuanxu@uchicago.edu>
2023-01-04 17:08:55 -0500

    print more info

--- PairTrading/trading/manager.py
+++ PairTrading/trading/manager.py
@@ -157,7 +157,7 @@ class TradingManager(Base, metaclass=Singleton):
             
             bidAskSpread = (abs(quote0.bid_price - quote0.ask_price)/quote0.ask_price + abs(quote1.bid_price - quote1.ask_price)/quote1.ask_price) / 2
             
-            logger.info(f"{pair[0]}--{pair[1]}, curr_profit: {round(currProfit*100, 2)}%, bid_ask_spread: {bidAskSpread}")
+            logger.info(f"{pair[0]}--{pair[1]}, curr_profit: {round(currProfit*100, 2)}%, bid_ask_spread: {bidAskSpread}, zscore: {self.kalmanEngine.zscore}")
             if float(positions[0].unrealized_plpc) + float(positions[1].unrealized_plpc) >= bidAskSpread*10 and \
                 self.kalmanEngine.canExit():
                 res.append(pair)
