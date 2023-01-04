7cd563204e7e386cf7827e191c43178460166f1b
John Xu <haoxuanxu@uchicago.edu>
2023-01-04 13:55:59 -0500

    change printing

--- PairTrading/trading/manager.py
+++ PairTrading/trading/manager.py
@@ -143,7 +143,7 @@ class TradingManager(Base, metaclass=Singleton):
             
             currProfit:float = float(positions[0].unrealized_plpc) + float(positions[1].unrealized_plpc)
             
-            logger.info(f"{pair[0]}--{pair[1]}: curr_profit: {float(positions[0].unrealized_plpc)*100}% -- {float(positions[1].unrealized_plpc)*100}%")
+            logger.info(f"{pair[0]}--{pair[1]}: curr_profit: {round(currProfit*100, 4)}%")
             if float(positions[0].unrealized_plpc) + float(positions[1].unrealized_plpc) > 0.05:
                 res.append(pair)
             else:   
