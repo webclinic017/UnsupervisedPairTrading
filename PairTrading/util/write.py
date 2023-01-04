75ec40dbfb3bf798333c102394420fbcee38a9b2
John Xu <haoxuanxu@uchicago.edu>
2023-01-04 09:37:58 -0500

    datetime to string

--- PairTrading/util/write.py
+++ PairTrading/util/write.py
@@ -25,6 +25,6 @@ def dumpTradingRecord(record:dict[tuple, float]) -> None:
     
 def dumpRecentlyClosed(recentlyClosed:dict[str, date]) -> None:
     for symbol, submitTime in recentlyClosed.items():
-        recentlyClosed[symbol] = submitTime
+        recentlyClosed[symbol] = submitTime.strftime("%Y-%m-%d")
         
     writeToJson(recentlyClosed, "saveddata/recently_closed.json")
