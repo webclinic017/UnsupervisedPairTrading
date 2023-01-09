317c72f71a1b316cc53ebe45273f4791e138e4a4
John Xu <haoxuanxu@uchicago.edu>
2023-01-09 10:12:18 -0500

    record days elapsed

--- PairTrading/trading/manager.py
+++ PairTrading/trading/manager.py
@@ -141,13 +141,13 @@ class TradingManager(Base, metaclass=Singleton):
         for pair, positions in openedPairsPositions.items():
             
             currProfit:float = (float(positions[0].unrealized_plpc) + float(positions[1].unrealized_plpc)) / 2
-            
-            logger.info(f"{pair[0]}--{pair[1]}, curr_profit: {round(currProfit*100, 2)}%")
+            ordersList:list[Order] = self.tradingClient.getPairOrders(pair)
+            daysElapsed:int = (date.today() - ordersList[0].submitted_at.date()).days
+            logger.info(f"{pair[0]}--{pair[1]}, curr_profit: {round(currProfit*100, 2)}%, days_elapsed: {daysElapsed}")
             if currProfit >= 0.1:
                 res.append(pair)
-            else:   
-                ordersList:list[Order] = self.tradingClient.getPairOrders(pair)
-                if (date.today() - ordersList[0].submitted_at.date()).days > 30:
+            else:                 
+                if daysElapsed > 30:
                     res.append(pair)
         return res 
     
