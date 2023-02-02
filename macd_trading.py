930b1e0b97d72b8342dfa7be613bd8cc0c9d5136
John Xu <haoxuanxu@uchicago.edu>
2023-02-02 16:17:13 -0500

    open 10 minutes before close

--- macd_trading.py
+++ macd_trading.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         
         
     # wait till 10 minutes before the market closes
-    secondsLeft:int = int((manager.tradingClient.clock.next_close.replace(tzinfo=None) - relativedelta(minutes=15) -
+    secondsLeft:int = int((manager.tradingClient.clock.next_close.replace(tzinfo=None) - relativedelta(minutes=10) -
                        manager.tradingClient.clock.timestamp.replace(tzinfo=None)).total_seconds())
     logger.info(f"{round(secondsLeft/3600, 2)} hours left before the bot can start operating")   
     for _ in tqdm(range(secondsLeft), desc="waiting till the bot can act ..."):
